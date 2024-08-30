9877eeb98c07175a66fd853c84a69fb8ccb6f25b
isabel-git-hub <wonyuchung@gmail.com>
2024-08-30 17:08:51 +0900

    240830 wall 코드 작성 완료 (css, img 수정 필요)

--- AurayStudio/src/main/java/com/example/AurayStudio/controller/WallController.java
+++ AurayStudio/src/main/java/com/example/AurayStudio/controller/WallController.java
@@ -24,89 +24,152 @@ public class WallController {
 		model.addAttribute("title", "스타일월");
 		return "product/wall/style";
 	}
-
-	// 스타일월 - Acacia
-	@GetMapping("/style/acacia")
-	public String wallStyleAcacia(Model model) {
-		model.addAttribute("title", "acacia");
-		return "product/wall/style/acacia";
+	
+	// 스타일월 - Marble
+	@GetMapping("/style/marble/1")
+	public String wallStyleMarble1(Model model) {
+		model.addAttribute("title", "마블 제품 상세");
+		return "product/wall/style/marble_1";
 	}
 
-	// 스타일월 - Soft Oak
-	@GetMapping("/style/softoak")
-	public String wallStyleSoftOak(Model model) {
-		model.addAttribute("title", "softoak");
-		return "product/wall/style/softoak";
+	// 스타일월 - Marble
+	@GetMapping("/style/marble/2")
+	public String wallStyleMarble2(Model model) {
+		model.addAttribute("title", "마블 제품 상세");
+		return "product/wall/style/marble_2";
 	}
 
-	// 스타일월 - Wood Noir
-	@GetMapping("/style/woodnoir")
-	public String wallStyleWoodNoir(Model model) {
-		model.addAttribute("title", "woodnoir");
-		return "product/wall/style/woodnoir";
+	// 스타일월 - Marble
+	@GetMapping("/style/marble/3")
+	public String wallStyleMarble3(Model model) {
+		model.addAttribute("title", "마블 제품 상세");
+		return "product/wall/style/marble_3";
 	}
 
-	// 스타일월 - Ekaterina
-	@GetMapping("/style/ekaterina")
-	public String wallStyleEkaterina(Model model) {
-		model.addAttribute("title", "ekaterina");
-		return "product/wall/style/ekaterina";
+	// 스타일월 - Wood
+	@GetMapping("/style/wood/1")
+	public String wallStyleWood1(Model model) {
+		model.addAttribute("title", "우드 제품 상세");
+		return "product/wall/style/wood_1";
 	}
 	
-	// 스타일월 - Milky Breeze
-	@GetMapping("/style/milkybreeze")
-	public String wallStyleMilkyBreeze(Model model) {
-		model.addAttribute("title", "milkybreeze");
-		return "product/wall/style/milkybreeze";
-	}
-	
-	// 스타일월 - Rone Light
-	@GetMapping("/style/ronelight")
-	public String wallStyleRoneLight(Model model) {
-		model.addAttribute("title", "ronelight");
-		return "product/wall/style/ronelight";
-	}
-	
-	// 스타일월 - Charcoal Stone
-	@GetMapping("/style/charcoalstone")
-	public String wallStyleCharcoalStone(Model model) {
-		model.addAttribute("title", "charcoalstone");
-		return "product/wall/style/charcoalstone";
+	// 스타일월 - Wood
+	@GetMapping("/style/wood/2")
+	public String wallStyleWood2(Model model) {
+		model.addAttribute("title", "우드 제품 상세");
+		return "product/wall/style/wood_2";
 	}
 
-	// 스타일월 - Colosseo Brown
-	@GetMapping("/style/colosseobrown")
-	public String wallStyleColosseoBrown(Model model) {
-		model.addAttribute("title", "colosseobrown");
-		return "product/wall/style/colosseobrown";
+	// 스타일월 - Wood
+	@GetMapping("/style/wood/3")
+	public String wallStyleWood3(Model model) {
+		model.addAttribute("title", "우드 제품 상세");
+		return "product/wall/style/wood_3";
 	}
 	
-	// 스타일월 - Matte Marmo
-	@GetMapping("/style/mattemarmo")
-	public String wallStyleMatteMarmo(Model model) {
-		model.addAttribute("title", "mattemarmo");
-		return "product/wall/style/mattemarmo";
+	// 스타일월 - Etc
+	@GetMapping("/style/etc/1")
+	public String wallStyleEtc1(Model model) {
+		model.addAttribute("title", "기타 제품 상세");
+		return "product/wall/style/etc_1";
 	}
 
-	// 스타일월 - Urban Grey
-	@GetMapping("/style/urbangrey")
-	public String wallStyleUrbanGrey(Model model) {
-		model.addAttribute("title", "urbangrey");
-		return "product/wall/style/urbangrey";
+	// 스타일월 - Etc
+	@GetMapping("/style/etc/2")
+	public String wallStyleEtc2(Model model) {
+		model.addAttribute("title", "기타 제품 상세");
+		return "product/wall/style/etc_2";
 	}
 	
-	// 스타일월 - Sugar Marble
-	@GetMapping("/style/sugarmarble")
-	public String wallStyleSugarMarble(Model model) {
-		model.addAttribute("title", "sugarmarble");
-		return "product/wall/style/sugarmarble";
-	}
-
 	// wall - 월판넬
 	@GetMapping("/panel")
 	public String wallpanel(Model model) {
-		model.addAttribute("title", "일반창");
+		model.addAttribute("title", "월판넬");
 		return "product/wall/panel";
 	}
 
+	// 스타일월 - Wall-system
+	@GetMapping("/panel/wall-system/1")
+	public String wallPanelWallsystem1(Model model) {
+		model.addAttribute("title", "월시스템 제품 상세");
+		return "product/wall/panel/wall-system_1";
+	}
+	
+	// 스타일월 - Wall-system
+	@GetMapping("/panel/wall-system/2")
+	public String wallPanelWallsystem2(Model model) {
+		model.addAttribute("title", "월시스템 제품 상세");
+		return "product/wall/panel/wall-system_2";
+	}
+	
+	// 스타일월 - Wall-system
+	@GetMapping("/panel/wall-system/3")
+	public String wallPanelWallsystem3(Model model) {
+		model.addAttribute("title", "월시스템 제품 상세");
+		return "product/wall/panel/wall-system_3";
+	}
+	
+	// 스타일월 - Tamba
+	@GetMapping("/panel/tamba/1")
+	public String wallPanelTamba1(Model model) {
+		model.addAttribute("title", "템바 제품 상세");
+		return "product/wall/panel/tamba_1";
+	}
+	
+	// 스타일월 - Tamba
+	@GetMapping("/panel/tamba/2")
+	public String wallPanelTamba2(Model model) {
+		model.addAttribute("title", "템바 제품 상세");
+		return "product/wall/panel/tamba_2";
+	}	
+	
+	// 스타일월 - Board
+	@GetMapping("/panel/board/1")
+	public String wallPanelBoard1(Model model) {
+		model.addAttribute("title", "디자인보드 제품 상세");
+		return "product/wall/panel/board_1";
+	}	
+	
+	// 스타일월 - Classic
+	@GetMapping("/panel/classic/1")
+	public String wallPanelClassic1(Model model) {
+		model.addAttribute("title", "클래식월 제품 상세");
+		return "product/wall/panel/classic_1";
+	}	
+	
+	// 스타일월 - Shelf
+	@GetMapping("/panel/shelf/1")
+	public String wallPanelShelf1(Model model) {
+		model.addAttribute("title", "벽선반 제품 상세");
+		return "product/wall/panel/shelf_1";
+	}	
+	
+	// 스타일월 - Shelf
+	@GetMapping("/panel/shelf/2")
+	public String wallPanelShelf2(Model model) {
+		model.addAttribute("title", "벽선반 제품 상세");
+		return "product/wall/panel/shelf_2";
+	}
+	
+	// 스타일월 - Shelf
+	@GetMapping("/panel/shelf/3")
+	public String wallPanelShelf3(Model model) {
+		model.addAttribute("title", "벽선반 제품 상세");
+		return "product/wall/panel/shelf_3";
+	}	
+	
+	// 스타일월 - Shelf
+	@GetMapping("/panel/shelf/4")
+	public String wallPanelShelf4(Model model) {
+		model.addAttribute("title", "벽선반 제품 상세");
+		return "product/wall/panel/shelf_4";
+	}		
+	
+	// 스타일월 - Art
+	@GetMapping("/panel/art/1")
+	public String wallPanelArt1(Model model) {
+		model.addAttribute("title", "아트월 제품 상세");
+		return "product/wall/panel/art_1";
+	}
+	
 }
